0845fd25a4a74e2c53f5982821bd9c66bc8ff505
erocher88 <erocher88@outlook.com>
2023-09-12 22:34:32 -0600

    Changed and simplified MyAtoi. Test accepted, memory usage ok but slow

--- leetCodeExercises/src/leetCodeExercises/MyAtoi.java
+++ leetCodeExercises/src/leetCodeExercises/MyAtoi.java
@@ -2,40 +2,30 @@ package leetCodeExercises;
 
 public class MyAtoi {
 	public static int stringToInt(String s) {
-		int flagSign = 1, flagStop = 0, flagEmpty = 0;
+		int sign = 1;
 		long answer = 0;
 		
-		for (int i = 0; i < s.length(); i++) {			
-			if (answer * flagSign > Integer.MAX_VALUE || answer * flagSign < Integer.MIN_VALUE) {
+		for (int i = 0, flagStop = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ' && flagStop == 0) {
+				continue;
+			} else if (flagStop == 0 && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+				sign = (s.charAt(i) == '-') ? -1 : sign;
+				flagStop = 1;
+			} else if (Character.isDigit(s.charAt(i)) == false) {
 				break;
-			}
-			if (flagStop == -1 || s.charAt(i) == '.') break;
-			flagSign = (s.charAt(i) != '-' || Character.isDigit(s.charAt(i))) ? flagSign : -1;
-			
-			if (Character.isDigit(s.charAt(i))) {
+			} else if (Character.isDigit(s.charAt(i))) {
 				answer = answer * 10 + Long.parseLong("" + s.charAt(i));
 				flagStop = 1;
-				flagEmpty = 1;
-			} else if (s.charAt(i) == ' ') {
-				flagStop = (flagEmpty == 1) ? -1 : flagStop;
-			} else if (Character.isDigit(s.charAt(i)) == false) {
-				flagStop = (flagStop == 1) ? -1 : flagStop;
-				flagEmpty = 1;
 			}
+			if (answer * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+			if (answer * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
 		}
-		answer *= flagSign;
-		if (answer > Integer.MAX_VALUE) {
-			return Integer.MAX_VALUE;
-		} else if (answer < Integer.MIN_VALUE) {
-			return Integer.MIN_VALUE;
-		} else {
-			return (int)answer;
-		}
+		return (int)answer*sign;
 	}
 	
 	public static void main(String[] args) {
 		long begin = System.nanoTime();
-		int answer = stringToInt("   sdfsdf  dfgdgdfg32848987.1");
+		int answer = stringToInt("       -7687687986978");
 		long end = System.nanoTime();
 		double time = end - begin;
 		System.out.println(answer);
