570055567e6537f928d9bfd7fb006e56f8f3af3b
erocher88 <erocher88@outlook.com>
2023-09-20 21:29:42 -0600

    second attempt, subPatternMatch and isMatch redone, case pattern .*c returns true instead of false

--- leetCodeExercises/src/leetCodeExercises/ExpressionMatch.java
+++ leetCodeExercises/src/leetCodeExercises/ExpressionMatch.java
@@ -3,18 +3,19 @@ package leetCodeExercises;
 import java.util.ArrayList;
 
 public class ExpressionMatch {
-	
+
 	public static ArrayList<String> stringToArray(String s) {
 		ArrayList<String> result = new ArrayList<String>();
 		StringBuilder buffer = new StringBuilder();
-		
+
 		for (int i = 0; i < s.length(); i++) {
 			if (s.charAt(i) != '*' && i != s.length() - 1) {
 				buffer.append(s.charAt(i));
 			} else if (s.charAt(i) == '*') {
-				String pivot = buffer.charAt(buffer.length() -1) + "*";
+				String pivot = buffer.charAt(buffer.length() - 1) + "*";
 				buffer.deleteCharAt(buffer.length() - 1);
-				if (buffer.length() != 0) result.add(buffer.toString());
+				if (buffer.length() != 0)
+					result.add(buffer.toString());
 				result.add(pivot);
 				buffer.setLength(0);
 			} else if (i == s.length() - 1) {
@@ -25,42 +26,61 @@ public class ExpressionMatch {
 		}
 		return result;
 	}
-	
-	//s is input string, p is sub pattern, modify to return an int and add parameter index
-	public static boolean subPatternMatch(String s, String p) {
-		if (p.length() > s.length()) return false;
-		int count = s.length();
-		for (int i = 0; i < p.length(); i++) {
-			if (p.charAt(i) == '*') return true;
-			if (p.charAt(i) == '.' || (p.charAt(i) == s.charAt(i))) {
-				count--;
-			} else if (p.charAt(i) != s.charAt(i)) {
-				return false;
+
+//s is input string, p is sub pattern
+	public static int subPatternMatch(String s, String p, int index) {
+		if (p.contains("*")) {
+			if (p.charAt(0) == '.') {
+				while (index < s.length()) {
+					index++;
+				}
+				return index;
+			} else {
+				while (index < s.length() && p.charAt(0) == s.charAt(index)) {
+					index++;
+				}
+				return index;
+			}
+		} else {
+			int counter = 0;
+			while (counter < p.length() && index < s.length()
+					&& (p.charAt(counter) == s.charAt(index) || p.charAt(counter) == '.')) {
+				counter++;
+				index++;
+			}
+			if (counter == p.length()) {
+				return index;
+			} else {
+				return -1;
 			}
 		}
-		if (count == 0) {
+	}
+
+//s is input string, p is pattern
+	public static boolean isMatch(String s, String p) {
+		// if (!p.contains(".") && !p.contains("*") && !s.equals(p)) {
+		// return false;
+		ArrayList<String> subPatterns = stringToArray(p);
+		int index = 0, arrayCounter = 0;
+
+		while (index != -1 && index < s.length() && arrayCounter < subPatterns.size()) {
+			index = subPatternMatch(s, subPatterns.get(arrayCounter), index);
+			arrayCounter++;
+		}
+		if (index == s.length()) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
-	//s is input string, p is pattern
-	public static boolean isMatch(String s, String p) {
-		if (!p.contains(".") && !p.contains("*") && !s.equals(p)) {
-			return false;
-		}
-		ArrayList<String> subPatterns = stringToArray(p);
-		for (int i = 0; i < subPatterns.size(); i++) {
-			if (subPatternMatch(s, subPatterns.get(i))) return true;
-		}
-		return false;
-	}
-	
+
 	public static void main(String[] args) {
-		boolean answer = isMatch("ab", "b*");
-		ArrayList<String> test = stringToArray("mis*is.");
-		System.out.println(test);
-		System.out.println(answer);
+		ArrayList<String> test1 = stringToArray(".*c");
+		int test2 = subPatternMatch("mississippi", "mi", 0);
+		boolean test3 = isMatch("ab", ".*c");
+		System.out.println(test1);
+		System.out.println(test2);
+		System.out.println(test3);
 	}
+
 }
